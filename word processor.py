74815ef27d19f5cf1a9e556a183e699861f7df46
Bondalapati Jyothi Swaroop <jyothiswaroop1811@gmail.com>
2026-05-05 12:03:56 +0530

    Update word processor.py

--- word processor.py
+++ word processor.py
@@ -1,19 +1,22 @@
-def word_processor():
+def word_processor()
     text = input("Enter sentence: ")
 
     vowels = "aeiou"
     count = 0
 
-    for ch in text.lower()
-        if ch in vowels:
-            count += 0
+    for ch in text.lower
+        if ch in vowels
+            count = count + 1
 
-   
-    words = text.split()
+    print("Vowel count:" count)
 
-    print("Words with more than 3 letters:")
+
+    words = text.split
+
+    print("Words with more than 3 letters")
+
     for w in words:
-        if len() > 3:
-            print()
+        if len(w) > 3
+            print w
 
-
+word_processor
