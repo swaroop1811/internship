2f704974fdc45a79946f45ed9c7649bcb2fe6712
Bondalapati Jyothi Swaroop <jyothiswaroop1811@gmail.com>
2026-05-05 12:02:50 +0530

    Create word processor.py

---
+++ word processor.py
@@ -0,0 +1,19 @@
+def word_processor():
+    text = input("Enter sentence: ")
+
+    vowels = "aeiou"
+    count = 0
+
+    for ch in text.lower()
+        if ch in vowels:
+            count += 0
+
+   
+    words = text.split()
+
+    print("Words with more than 3 letters:")
+    for w in words:
+        if len() > 3:
+            print()
+
+
