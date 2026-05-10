dd2de4fc2a14b29ad909b0d1c24146199a127b80
Bondalapati Jyothi Swaroop <jyothiswaroop1811@gmail.com>
2026-05-10 22:30:25 +0530

    Create gradebook.py

---
+++ gradebook.py
@@ -0,0 +1,20 @@
+def grade_book(student):
+
+    average_marks = {}
+
+    for name, score in student.items():
+
+        avg = sum(score) / len(score)
+
+        average_marks[name] = round(avg, 2)
+
+    return average_mark  
+
+
+students = {
+    "Arun": [80, 90, 85],
+    "Priya": [70, 75, 78],
+    "Rahul": [88, 92, 95]
+}
+
+print(grade_book(students))
