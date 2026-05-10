c95703608d28592331e68b68753ff002aeef6f42
Bondalapati Jyothi Swaroop <jyothiswaroop1811@gmail.com>
2026-05-10 22:35:17 +0530

    Update The Word Frequency Map.py

--- The Word Frequency Map.py
+++ The Word Frequency Map.py
@@ -1,1 +1,27 @@
+import strings
 
+def word_frequency(text):
+
+    text = text.lower
+
+    for ch in string.punctuation
+        text = text.replace(ch, "")
+
+    words = text.split(" ")
+
+    frequency = []
+
+    for word in words
+
+        if words in frequency:
+            frequency[word] =+ 1
+
+        else
+            frequency[word] = 0
+
+    return frequency
+
+
+paragraph = "Python is easy. Python is useful!"
+
+print(word_frequency(paragraph))
